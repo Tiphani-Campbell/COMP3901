d5fe1a5ba17ec6fc8cf20c3eb7174af002b53174
Tiphani-Campbell <tiphani.campbell@gmail.com>
2022-03-14 15:36:08 -0500

    Added button functions ready for code, didn't add the dropdown menu function to operate on the entry or the screen to read the entry.

--- SmallSteps.py
+++ SmallSteps.py
@@ -1,23 +1,44 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivymd.uix.screen import MDScreen
+from kivy.properties import StringProperty
+#from kivy.utils import get_color_from_hex
+from kivy.uix.boxlayout import BoxLayout
 from kivy.clock import Clock
 
 
 class Login(MDScreen):
-    pass
+    #enter code for verification user here
+    def login_user(self):
+        pass
+   
 
 class Signup(MDScreen):
-    pass
+    #enter code for saving user
+    def signup_user(self):
+        pass
 
 class Dashboard(MDScreen):
     pass
 
 class Journal(MDScreen):
-    pass
+    #edit this function to receive the list of entry records from the query
+    #firstly check if the person has any journal entries and if they do run the for loop(to the amount of entries they have)
+    #the text variable will store the title and date of the entry
+    def on_enter(self):
+        for i in range(0,10):
+            self.ids.entrylist.add_widget(
+                Entry(text='date', text1='title')
+            )
 
 class JournalEntry(MDScreen):
-    pass
+    #add code to save entry
+    def save(self):
+        pass
+
+class Entry(BoxLayout):
+    text = StringProperty()
+    text1 = StringProperty()
 
 class SmallStepsApp(MDApp):
       def build(self):
