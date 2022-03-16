825e70dce844271a069f48067c4f387997e043f4
Tiphani-Campbell <tiphani.campbell@gmail.com>
2022-03-16 15:23:39 -0500

    Made slight changes to button press functions

--- SmallSteps.py
+++ SmallSteps.py
@@ -2,21 +2,37 @@ from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivymd.uix.screen import MDScreen
 from kivy.properties import StringProperty
+from kivymd.toast import toast
+from kivy import platform
 #from kivy.utils import get_color_from_hex
 from kivy.uix.boxlayout import BoxLayout
 from kivy.clock import Clock
-
+    
 
 class Login(MDScreen):
     #enter code for verification user here
     def login_user(self):
-        pass
+        username = self.username.text #this is how you get text from the fields. it's self.<textfield id>.text
+        if username != "": #edit this to check for username and password verfifcation
+            self.manager.transition.direction = "left"
+            self.manager.current = "dashboard"  
+        else:# this section is to display an invalid message for a short period of time at the bottom of the screen
+            if platform == "android":
+                toast("Invalid password or username", gravity=80)# gravity=80 is to ensure it pops up at the bottom of the screen for the phone, it would pop up in the center if this wasn't here
+            else:
+                toast("Invalid password or username")
    
 
 class Signup(MDScreen):
     #enter code for saving user
+    #Required fields are set on the page so check if they're empty and do the toast thing I did in the login class for any errors
+    #Ensure, when you are using the toast method to flash a message on the screen, to test for the platfrom being android, the else was for it to display on the laptop
     def signup_user(self):
-        pass
+
+
+        self.manager.transition.direction = "right" #anything with self.manager is responsible for changing the screen
+        self.manager.current = "login"
+        
 
 class Dashboard(MDScreen):
     pass
@@ -34,6 +50,8 @@ class Journal(MDScreen):
 class JournalEntry(MDScreen):
     #add code to save entry
     def save(self):
+        self.manager.transition.direction = "right"
+        self.manager.current = "journal"
         pass
 
 class Entry(BoxLayout):
